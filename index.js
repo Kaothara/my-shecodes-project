2cccb73de20d0faccb4cdaba982d616541c105de
Kaothara <109860702+Kaothara@users.noreply.github.com>
2022-12-21 15:46:25 +0100

    updated the previous work of the weather app

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-alert("Hello");
+let now = new Date();
+let date = now.getDate();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let year = now.getFullYear();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let day = days[now.getDay()];
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+let dateSpan = document.querySelector("span");
+dateSpan.innerHTML = `${day} ${month} ${date}, ${hours} : ${minutes}, ${year}`;
+
+// challenge 2
+function citySearch(event) {
+  event.preventDefault();
+  let cityName = document.querySelector("#city");
+  let cityNameSearch = document.querySelector("#city-form");
+
+  cityName.innerHTML = cityNameSearch.value;
+}
+let newCity = document.querySelector("#city-form");
+newCity.addEventListener("submit", citySearch);
+
+// challenge 3
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureValue = document.querySelector("#temperature-value");
+  temperatureValue.innerHTML = 25;
+}
+function convertToCelcius(event) {
+  event.preventDefault();
+  let temperatureValue = document.querySelector("#temperature-value");
+  temperatureValue.innerHTML = 32;
+}
+let fahrenheitValue = document.querySelector("#fahrenheit-value");
+fahrenheitValue.addEventListener("click", convertToFahrenheit);
+
+let celciusValue = document.querySelector("#celcius-value");
+celciusValue.addEventListener("click", convertToCelcius);
